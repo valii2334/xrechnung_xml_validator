bfb5dc43103a0336f4dfe38b191667fe9378bb04
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2024-12-15 16:36:20 +0200

    use shellwords

--- lib/xrechnung_xml_validator/xml_validator.rb
+++ lib/xrechnung_xml_validator/xml_validator.rb
@@ -15,8 +15,19 @@ module XrechnungXmlValidator
 
     private
 
+    # jar => which jar executable to use
+    # s   => which scenarios to use against the provided xml
+    # r   => in which repository we find the validation files
+    # h   => xrechnung xml file path
+    # o   => output directory for HTML report
     def validation_response
-      `java -jar #{XrechnungXmlValidator::FilePaths.xrechnung_validator_jar} -s #{XrechnungXmlValidator::FilePaths.xrechnung_validator_scenarios} -r #{XrechnungXmlValidator::FilePaths.xrechnung_validator_directory} -h #{absolute_path} -o #{output_directory}`
+      `
+        java -jar #{Shellwords.escape(XrechnungXmlValidator::FilePaths.xrechnung_validator_jar)}       \
+             -s   #{Shellwords.escape(XrechnungXmlValidator::FilePaths.xrechnung_validator_scenarios)} \
+             -r   #{Shellwords.escape(XrechnungXmlValidator::FilePaths.xrechnung_validator_directory)} \
+             -h   #{Shellwords.escape(absolute_path)}                                                  \
+             -o   #{Shellwords.escape(output_directory)}
+      `
     end
 
     def raise_xrechnung_error
--- xrechnung_xml_validator.gemspec
+++ xrechnung_xml_validator.gemspec
@@ -29,6 +29,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "faker"
   spec.add_development_dependency "pry"
+  spec.add_development_dependency "shellwords"
 
   spec.required_ruby_version = Gem::Requirement.new(">= 3.1.0")
 end
